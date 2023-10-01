ff7afce4be03125c521aeb62bbd1449a37166843
Elisson Silva <elisson.asilva@mercadolivre.com.br>
2023-09-30 21:15:31 -0300

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'green': '#247356',
+        'green-dark': '#1C6147',
+        'gray': '#A9A9A9',
+        'gray-light': '#d3dce6',
+        'white': '#FFFCFF',
+        'black': '#1E1E1E',
+        'yellow': '#BC9212'
+      },
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      extend: {}
+    },
+  },
+  plugins: [],
+}
+export default config
